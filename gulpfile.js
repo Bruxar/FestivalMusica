a95ea948a9cfb40b5ce59a6d7aeac7776ce1acdb
Bruxar <larancibia@utem.cl>
2024-01-08 20:47:52 -0300

    Transformando tipo img con gulp y lineup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require('gulp'); // Importar gulp
+const { src, dest, watch, parallel } = require('gulp'); // Importar gulp
+
+// CSS - SASS
 const sass = require('gulp-sass')(require('sass')); // Importar gulp-sass
 const plumber = require('gulp-plumber'); // Importar gulp-plumber
+
+//Imagenes
+const cache = require('gulp-cache'); // Importar gulp-cache
+const imagemin = require('gulp-imagemin'); // Importar gulp-imagemin
+const webp = require('gulp-webp'); // Importar gulp-webp
+const avif = require('gulp-avif'); // Importar gulp-avif
 
 function css(done){
 
@@ -16,10 +24,50 @@ function css(done){
     done(); // Callback que indica que la tarea ha terminado
 }
 
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'));
+    
+    done();
+}
+
+function versionWebp(done){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}') // Indicamos la ruta del archivo
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
+function versionAvif(done){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}') // Indicamos la ruta del archivo
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css) // Indicamos la ruta del archivo a observar y la tarea a ejecutar
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
